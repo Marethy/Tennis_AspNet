32f72ff517d2660fc2f5dbf5fb4119c92e1afc94
22520565 <22520565@gm.uit.edu.vn>
2025-04-12 16:33:13 +0700

    feat(oder): add order and product repo.

---
+++ Repositories/TokenRepository.cs
@@ -0,0 +1,16 @@
+﻿using Tennis.Interfaces;
+using Tennis.Models;
+
+namespace Tennis.Repositories;
+
+public class TokenRepository(TennisWebMVCContext context)
+    : ITokenRepository
+{
+    private readonly TennisWebMVCContext _context = context;
+
+    public bool CheckToken(string userName, string token)
+    {
+        return _context.Tokens.FirstOrDefault(Token =>
+            Token.CustomerUserName == userName && Token.TokenValue == token && Token.Expiry > DateTime.Now) != null;
+    }
+}
