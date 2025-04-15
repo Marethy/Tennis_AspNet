e613aa8761eb4b57c97ceef1864d30e1c9744f27
22520565 <22520565@gm.uit.edu.vn>
2025-04-15 23:06:55 +0700

    feat(blog): add Views.

---
+++ Views/Blog/Components/BlogAsideComponent/BlogAsideComponent.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Tennis.Interfaces;
+
+namespace Tennis.Views.Blog.Components.BlogAsideComponent;
+
+public class BlogAsideComponent(IBlogRepository repo) : ViewComponent
+{
+    private readonly IBlogRepository _repo = repo;
+
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        var obj = await _repo.GetListAsync();
+        return View("BlogAsideComponent", obj);
+    }
+}
---
+++ Views/Blog/Components/BlogCommentComponents/BlogCommentComponents.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Tennis.Interfaces;
+
+namespace Tennis.Views.Blog.Components.BlogCommentComponents;
+
+public class BlogCommentComponents(IBlogRepository repo) : ViewComponent
+{
+    private readonly IBlogRepository _repo = repo;
+
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        //@await Component.InvokeAsync("BlogCommentComponents")
+
+        var obj = await _repo.GetListAsync();
+        return View("BlogCommentComponents", obj);
+    }
+}
